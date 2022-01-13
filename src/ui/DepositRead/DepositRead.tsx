eb3d31554e84bd451c517e0dbae17c612212ce7c
Verkhohliad <ihor.verkhohliad@gmail.com>
2022-01-13 13:15:12 +0200

    read deposit and set to read-only info

--- src/ui/DepositRead/DepositRead.tsx
+++ src/ui/DepositRead/DepositRead.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 
 const DepositRead = ({ wallet, contract }) => {
-    // add read deposit
-
     const [deposit, setDeposit] = useState(0);
 
     useEffect(() => {
         const accountId = wallet.account().accountId;
 
-        console.log(accountId)
-
         contract.current.get_deposit_account_id({ account_id: accountId }).then(res => {
-            console.log({res});
-        })
+            console.log(`${accountId}: ${res}`);
+
+            setDeposit(res);
+        });
     }, []);
 
     return (
